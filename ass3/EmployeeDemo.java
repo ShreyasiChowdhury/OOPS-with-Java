1707fdd0748f6d96f393564ed03b5f02fb33b776
Shreyasi Chowdhury <85079050+ShreyasiChowdhury@users.noreply.github.com>
2022-08-15 12:27:03 +0530

    Add files via upload

---
+++ ass3/EmployeeDemo.java
@@ -0,0 +1,43 @@
+package com.company;
+import java.util.Scanner;
+class Employee
+{
+    Scanner sc=new Scanner(System.in);
+    private int pan;
+    private String name;
+    private double taxincome,tax;
+    void inputData()
+    {
+        System.out.println("Enter personal account number: ");
+        this.pan=sc.nextInt();
+        System.out.println("Enter name: ");
+        this.name=sc.next();
+        System.out.println("Enter taxable income: ");
+        this.taxincome=sc.nextDouble();
+        calculateData();
+    }
+    void calculateData()
+    {
+        if(taxincome<=250000)
+            tax=0;
+        else if(taxincome>=250001 && taxincome<=500000)
+            tax=taxincome*0.1;
+        else if(taxincome>=500001 && taxincome<=1000000)
+            tax=taxincome*0.2 + 30000;
+        else if(taxincome>=1000001)
+            tax=taxincome*0.3 + 50000;
+    }
+    void outputData()
+    {
+        System.out.println("PAN: "+pan+"\nName: "+name+"\nTax Income: "+taxincome+"\nTax: "+tax);
+    }
+}
+public class EmployeeDemo
+{
+    public static void main(String[] args)
+    {
+        Employee obj=new Employee();
+        obj.inputData();
+        obj.outputData();
+    }
+}

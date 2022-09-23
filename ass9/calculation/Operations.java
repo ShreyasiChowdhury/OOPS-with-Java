2d2a42fd12a153a194ecc6517e2f46ff7d4c128f
Shreyasi Chowdhury <85079050+ShreyasiChowdhury@users.noreply.github.com>
2022-09-24 00:15:17 +0530

    Create Operations.java

---
+++ ass9/calculation/Operations.java
@@ -0,0 +1,20 @@
+package calculation;
+public class Operations
+{
+    public static int sum(int a,int b)
+    {
+        return a+b;
+    }
+    public static int multiply(int a,int b)
+    {
+        return a*b;
+    }
+    public static double sum(double a,double b)
+    {
+        return a+b;
+    }
+    public static double multiply(double a,double b)
+    {
+        return a*b;
+    }
+}

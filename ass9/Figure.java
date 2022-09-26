ed42fc5a32b004700fd3bf6cc441a28ea8eb249c
Shreyasi Chowdhury <85079050+ShreyasiChowdhury@users.noreply.github.com>
2022-09-26 18:17:33 +0530

    Add files via upload

---
+++ ass9/Figure.java
@@ -0,0 +1,10 @@
+import mypack.*;
+class Figure
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Area of Rectangle: "+Shape.getArea(3,5));
+        System.out.println("Area of Square: "+Shape.getArea(4));
+        System.out.println("Area of Circle: "+Shape.getArea(2.0));
+    }
+}

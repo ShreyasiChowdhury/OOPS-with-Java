282998a95b10ab676728a3606c6cb90f48e96784
Shreyasi Chowdhury <85079050+ShreyasiChowdhury@users.noreply.github.com>
2022-11-23 03:34:21 +0530

    Add files via upload

---
+++ ass9/test1/C.java
@@ -0,0 +1,13 @@
+package test1;
+
+public class C extends A {
+    public static void main(String[] args) {
+        C obj = new C();
+        obj.update();
+    }
+    void update()
+    {
+        num3=5;
+        System.out.println(num3);
+    }
+}
